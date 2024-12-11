7680adc9a8dbc3baba218d97d0ce24808415f5d3
iDaryna <dargud99@gmail.com>
2024-12-11 17:46:55 -0500

    done

--- src/main/java/com/apress/controller/PollController.java
+++ src/main/java/com/apress/controller/PollController.java
@@ -1,63 +1,92 @@
 package com.apress.controller;
 
-import com.apress.domain.Poll;
-import com.apress.repository.PollRepository;
+import java.net.URI;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.Optional;
+import com.apress.domain.Poll;
+import com.apress.exception.ResourceNotFoundException;
+import com.apress.repository.PollRepository;
 
 @RestController
 public class PollController {
 
-    @Autowired
+    private final Logger logger = LoggerFactory.getLogger(PollController.class);
+
+    @Autowired // replaced for @inject
     private PollRepository pollRepository;
 
-
-    @RequestMapping(value = "/polls", method = RequestMethod.GET)
+    @GetMapping(value = "/polls")
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
+
         return new ResponseEntity<>(pollRepository.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/polls", method = RequestMethod.POST)
-    public ResponseEntity<?> createPoll(@RequestBody Poll poll){
-
+    @PostMapping(value="/polls" )
+    public ResponseEntity<?> createPoll(@Valid @RequestBody Poll poll) {
         poll = pollRepository.save(poll);
 
+        // Set the location header for the newly created resource
         HttpHeaders responseHeaders = new HttpHeaders();
-
         URI newPollUri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(poll.getId())
-                .toUri();
+                .path("/{id}").buildAndExpand(poll.getId()).toUri();
         responseHeaders.setLocation(newPollUri);
 
+        logger.info("Created a Poll successfully");
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
+    protected void verifyPoll(Long pollId) throws ResourceNotFoundException {
+        Poll poll = pollRepository.findById(pollId).orElse(null);
+        if(poll == null) {
+            throw new ResourceNotFoundException("Poll with id " + pollId + " not found");
+        }
+    }
+    @GetMapping(value="/polls/{pollId}")
     public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
-        Optional<Poll> p = pollRepository.findById(pollId);
+        verifyPoll(pollId);
+        Poll p = pollRepository.findById(pollId).orElse(null);
+
+//        logger.info("Got a Poll successfully");
         return new ResponseEntity<> (p, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
+    @PutMapping(value="/polls/{pollId}")
     public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
-        // Save the entity
-        Poll p = pollRepository.save(poll);
+        verifyPoll(pollId);
+        pollRepository.save(poll);
+
+        logger.info("Updated a Poll successfully");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
+    @DeleteMapping(value="/polls/{pollId}")
     public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        verifyPoll(pollId);
         pollRepository.deleteById(pollId);
+
+        logger.info("Deleted a Poll successfully");
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+
+
+
+
+
 }
--- src/main/java/com/apress/domain/Poll.java
+++ src/main/java/com/apress/domain/Poll.java
@@ -1,6 +1,8 @@
 package com.apress.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
@@ -9,41 +11,41 @@ public class Poll {
 
     @Id
     @GeneratedValue
-    @Column(name = "POLL_ID")
+    @Column(name="POLL_ID")
     private Long id;
 
     @Column(name="QUESTION")
+    @NotEmpty
     private String question;
 
     @OneToMany(cascade=CascadeType.ALL)
     @JoinColumn(name="POLL_ID")
     @OrderBy
+    @Size(min=2, max = 6)
     private Set<Option> options;
-
-    public Poll() {
-    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getQuestion() {
         return question;
     }
-
     public void setQuestion(String question) {
         this.question = question;
     }
-
     public Set<Option> getOptions() {
         return options;
     }
-
     public void setOptions(Set<Option> options) {
         this.options = options;
     }
+
+    @Override
+    public String toString() {
+        return getId() + ", " + getQuestion() + ", " + getOptions();
+    }
+
 }
